88893c595432808b3e435b1066c99aa5fa967f90
Leozindev_genius <leonardohenriquefr785@gmail.com>
2024-12-30 11:16:02 -0300

    primeiro do dia

--- Mine-Game/gamev1.py
+++ Mine-Game/gamev1.py
@@ -8,16 +8,25 @@ import os
 # Para tirar mensagens especificas
 os.environ['PYDEVD_DISABLE_FILE_VALIDATION'] = '1'
 
+
 # Função para colocar linhas no código
 def Linhas():
     print('\033[34m-\033[m' * 75)
 
 
-# Função pra título dos games
-def Title_game(txt):
+# Função para entrada dos games
+def Entrada_game(txt):
     print('[+] Entrando no game...')
     sleep(3) 
     Linhas()
+    if game == 1: # Para cada game irá tocar uma música diferente 
+        Music_game1()
+    elif game == 2:
+        Music_game2()
+    elif game == 3:
+        Music_game3()
+    elif game == 4:
+        Music_game4()
     print('\033[33m-=-\033[m' * 25)
     print(f'\033[4;35m{f"{txt}":^75}\033[m')
     print('\033[33m-=-\033[m' * 25)
@@ -44,12 +53,40 @@ def Empate():
     print('\033[33m-=-\033[m' * 15)
 
 
-# Função para tocar a musica do game
+# Função para tocar a musica principal
 def Music():
     pygame.mixer.init()
     pygame.mixer.music.load('/home/leonardo/Downloads/music-game.mp3')
     pygame.mixer.music.play()
     
+
+# Função para tocar musica de game 1
+def Music_game1():
+    pygame.mixer.init()
+    pygame.mixer.music.load('/home/leonardo/Downloads/music-game01.mp3')
+    pygame.mixer.music.play()
+
+
+# Função para tocar musica de game 2
+def Music_game2():
+    pygame.mixer.init()
+    pygame.mixer.music.load('/home/leonardo/Downloads/music-game02.mp3')
+    pygame.mixer.music.play()
+
+
+# Função para tocar musica de game 3
+def Music_game3():
+    pygame.mixer.init()
+    pygame.mixer.music.load('/home/leonardo/Downloads/music-game03.mp3')
+    pygame.mixer.music.play()
+
+
+# Função para tocar musica de game 4
+def Music_game4():
+    pygame.mixer.init()
+    pygame.mixer.music.load('/home/leonardo/Downloads/music-game04.mp3')
+    pygame.mixer.music.play()
+
 
 print("""\033[35m
 ███╗   ███╗██╗███╗   ██╗███████╗     ██████╗  █████╗ ███╗   ███╗███████╗
@@ -62,16 +99,42 @@ print("""\033[35m
 print(f'{"By Leozindev_genius":>75} \n\033[33m{"v1.0.0":>70}\033[m')
 Linhas()
 
-
+# Para tocar a música principal
 Music()
-print("""  \033[34m[ 1 JOKENPOL] / [ 2 IMPAR / PAR]\033[m / \033[32m[ 3 LOTERIA ] / [ 4 ACERTE O NÚMERO ]\033[m""") # Jogos disponiveis
+
+# Jogos disponiveis
+print("""  \033[34m[ 1 JOKENPOL] / [ 2 IMPAR / PAR]\033[m / \033[32m[ 3 LOTERIA ] / [ 4 ACERTE O NÚMERO ]\033[m \n  \033[31m[ 0 SAIR ]\033[m""")
 Linhas()
-while True:
+
+while True: # Loop infinito
     try:    
         game = int(input('[+] Escolha o que vai jogar: '))
-    except ValueError:
+
+    except ValueError: # Caso o usuário não digite um número inteiro valido
         print('\033[31mEscolha um número inteiro valido! Tente novamente.\033[m')
         continue
+
+    except Exception as e: # Para erros inesperados
+        print(e)
+        continue
+
+    if game == 0: # Para finalizar o game
+        print('Finalizando...')
+        sleep(3)
+        break
     
-    if game == 1:
-        Title_game('JOKENPOL')
+    elif game == 1: # Para entrar no modo JOKENPOL
+        Entrada_game('JOKENPOL')
+
+    elif game == 2: # Para entrar no modo IMPAR / PAR
+        Entrada_game('IMPAR / PAR')
+
+    elif game == 3: # Para entrar no modo LOTERIA
+        Entrada_game('LOTERIA')
+    
+    elif game == 4: # Para entrar no modo ACERTE O NÚMERO
+        Entrada_game('ACERTE O NÚMERO')
+
+    else: # Caso o usuário digite uma opção que não seja nenhuma das anteriores
+        print('\033[31mOpção invalida! Tente novamente.\033[m')
+        continue
