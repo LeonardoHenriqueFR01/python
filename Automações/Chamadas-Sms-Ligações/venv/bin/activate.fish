9e6289a8cec18775634ce4bd6ffa885fa4a33c2e
Leozindev_genius <leonardohenriquefr785@gmail.com>
2025-01-16 20:30:16 -0300

    ok

--- Automações/Chamadas-Sms-Ligações/venv/bin/activate.fish
+++ Automações/Chamadas-Sms-Ligações/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/leonardo/Documentos/projetos/Python/Automações/Chamadas -Sms-Ligações/venv"
+set -gx VIRTUAL_ENV "/home/leonardo/Documentos/projetos/Python/Automações/Chamadas-Sms-Ligações/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
